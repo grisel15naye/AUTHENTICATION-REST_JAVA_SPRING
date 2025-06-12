7551cf2cb8e591add200fd56b5585a0bcafcab0b
ramirezventuranayeli@gmail.com <ramirezventuranayeli@gmail.com>
2025-06-12 08:19:46 -0500

    Primer commit del proyecto Maven

---
+++ src/main/java/pe/company/Clase5SecurityApplication.java
@@ -0,0 +1,13 @@
+package pe.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase5SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase5SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/company/Clase5SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase5SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
